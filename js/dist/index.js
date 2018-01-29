b01891400fd16295890f519fb70af662150d470c
robertfairley <robert@robertfairley.com>
2018-01-28 22:29:04 -0500

    Fix repo structure

--- js/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const library_1 = require("./library");
-exports.l = new library_1.default({
-    storeFile: '/../db/libraryStore.json',
-});
